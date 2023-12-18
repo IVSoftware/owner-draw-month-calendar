07eb325343c5df06839c0391115c452d21853ff1
Tom Gregor <tgregor@ivsoftware.com>
2023-12-17 17:52:36 -0700

    Picker- Disallow drag on title bar

--- owner-draw-month-calendar/DecemberOnlyPicker.cs
+++ owner-draw-month-calendar/DecemberOnlyPicker.cs
@@ -36,7 +36,7 @@ namespace owner_draw_month_calendar
         }
         readonly PickerForm picker = new PickerForm();
     }
-    class PickerForm : Form, INotifyPropertyChanged
+    class PickerForm : Form, INotifyPropertyChanged, IMessageFilter
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -55,10 +55,22 @@ namespace owner_draw_month_calendar
                     }
                 }
             };
+            Application.AddMessageFilter(this);
+            Disposed += (sender, e) => Application.RemoveMessageFilter(this);
         }
         public DecemberOnlyCalendar Calendar { get; } = new DecemberOnlyCalendar
         {
             Dock = DockStyle.Fill,
         };
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HTCAPTION = 0x2;
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_NCLBUTTONDOWN && m.WParam.ToInt32() == HTCAPTION)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
